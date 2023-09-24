a9dfe6feec22dff0dbf576f66c964ce0359a639d
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-09-24 09:08:22 +0530

    remove case log from that sql file

--- src/migrations/scripts/20230904.sql
+++ src/migrations/scripts/20230904.sql
@@ -16,20 +16,6 @@ CREATE TABLE "ChildProfile" (
       REFERENCES "User"("Id") ON DELETE CASCADE
 );
 
-CREATE TABLE "Case" (
-  "Id" SERIAL PRIMARY KEY,
-  "CaseName" varchar(225) NOT NULL,
-  "State" varchar(10) NOT NULL,
-  "ChildProfileId" uuid NOT NULL,
-  "CaseOwnerId" uuid NOT NULL,
-  CONSTRAINT "FK_Case.CaseOwnerId"
-    FOREIGN KEY ("CaseOwnerId")
-      REFERENCES "User"("Id") ON DELETE CASCADE,
-  CONSTRAINT "FK_Case.ChildProfileId"
-    FOREIGN KEY ("ChildProfileId")
-      REFERENCES "ChildProfile"("Id") ON DELETE CASCADE
-);
-
 CREATE TYPE approval_status AS ENUM ('CREATED', 'DELETED', 'ACCEPT', 'REJECT');
 
 CREATE TABLE "ApprovalLog" (
@@ -106,13 +92,3 @@ CREATE TABLE "ProfileVersion" (
       REFERENCES "ChildProfile"("Id") ON DELETE CASCADE
 );
 
-CREATE TABLE "CaseLog" (
-  "Id" int PRIMARY KEY,
-  "CaseId" int NOT NULL,
-  "LogDescription" varchar(225) NOT NULL,
-  "LoggedDateTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "DocumentCollectionID" int,
-  CONSTRAINT "FK_CaseLog.CaseId"
-    FOREIGN KEY ("CaseId")
-      REFERENCES "Case"("Id") ON DELETE CASCADE
-);
