4c9871ae5e04345e3613b145fd0e81910535d134
Sandev <sandevdewthilina2000@gmail.com>
2023-09-28 19:58:25 +0530

    rcp bug

--- src/services/eventSubscribeService.js
+++ src/services/eventSubscribeService.js
@@ -1,16 +1,16 @@
 import { registerUserAsync } from "./userService.js";
 
 export const subscribeEvents = async (payload) => {
-  // parse data
-  payload = JSON.parse(data.content.toString());
-
   const { event, data } = payload;
 
   // manage event
   switch (event) {
     case "REGISTER_USER":
-      await registerUserAsync(data)
-      return 
+      try {
+        return await registerUserAsync(data);
+      } catch (e) {
+        return e.message;
+      }
     default:
       break;
   }
